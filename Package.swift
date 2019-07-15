3994e3bb24ef7b57c2ce5c9abbd3e53457309920
Patrick Luddy <pluddy@outlook.com>
2019-07-15 10:12:44 -0500

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OktaOidc",
+    products: [
+        .library(
+            name: "OktaOidc",
+            targets: ["OktaOidc"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "OktaOidc",
+            dependencies: []),
+    ]
+)
